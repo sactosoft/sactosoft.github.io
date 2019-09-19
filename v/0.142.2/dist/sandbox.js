32d44c23d06fc6c00ce1b1f2a2e6a3387952285d
Kripth <kripth04@gmail.com>
2019-09-19 22:09:08 +0200

    Update dependencies

--- v/0.142.2/dist/sandbox.js
+++ v/0.142.2/dist/sandbox.js
@@ -1,14 +1,14 @@
 /*! Transpiled from ./_src/dist/sandbox.sx using Sactory v0.142.2. Do not edit manually. */var ջթ=Sactory;var ջժ=ջթ.chain;var ջի={};Sactory.addWidget("message", function(message){var ջլ=ջթ.cfa(ջի, arguments, 1);
-	return ջժ(ջլ, [ջժ.create, "span", []], [ջժ.body, ջլ => {
+	return ջժ(ջլ, [ջժ.create, "span", 0, []], [ջժ.body, ջլ => {
 		ջթ.bind(ջլ, [message], [], (ջլ, ջխ) => {var [message]=ջխ.map(ջթ.value);
 			const match = message.matchAll(/`[^`]*`/g);
 			let curr, index = 0;
 			while(!(curr = match.next()).done) {
-				ջժ(ջլ, [ջժ.create, "f", [[ [5, "text", message.substring(index, index = curr.value.index)]]]], [ջժ.append] );
-				ջժ(ջլ, [ջժ.create, "code", [[ [5, "text", curr.value[0].slice(1, -1)]]]], [ջժ.append] );
+				ջժ(ջլ, [ջժ.create, "f", 0, [[5, "text", message.substring(index, index = curr.value.index)]]], [ջժ.append] );
+				ջժ(ջլ, [ջժ.create, "code", 0, [[5, "text", curr.value[0].slice(1, -1)]]], [ջժ.append] );
 				index += curr.value[0].length;
 			}
-			ջժ(ջլ, [ջժ.update, [[ [5, "text", message.substr(index)]]]] );
+			ջժ(ջլ, [ջժ.update, [[5, "text", message.substr(index)]]] );
 		});
 	}], [ջժ.append])
 });
@@ -180,7 +180,7 @@ class Sandbox {
 		const editor = this.initCodeMirror(element, this.readonly.value, "jsx");
 		editor.on("change", () => this.data.editorSource = editor.getValue());
 		this.data.source.$$subscribe(ջլ, value => editor.setValue(value), 8934);
-		ջժ(ջլ, [ջժ.use, element.nextElementSibling], [ջժ.update, [[ [3, ջթ.attr((ջթ.config.shortcut.save), ":prevent"), (event, target) => {this.save()}]]]] );
+		ջժ(ջլ, [ջժ.use, element.nextElementSibling], [ջժ.update, [[3, ջթ.attr((ջթ.config.shortcut.save), ":prevent"), (event, target) => {this.save()}]]] );
 		if(this.readonly.value) {
 			//TODO remove readOnly option
 			this.readonly.$$subscribe(ջլ, () => editor.setOption("readOnly", false));
@@ -193,84 +193,84 @@ class Sandbox {
 	}
 	
 	render({orientation = "x", hide = [], embedded}) {var ջլ=ջթ.cfa(ջի, arguments, 1);
-		return ջժ(ջլ, [ջժ.create, "div", [[ [0, "data-orientation", orientation]]]], [ջժ.body, ջլ => {
+		return ջժ(ջլ, [ջժ.create, "div", 0, [[0, "data-orientation", orientation]]], [ջժ.body, ջլ => {
 			ջթ.forEachArray(hide , (value) => {
-				ջժ(ջլ, [ջժ.update, [[ [5, "class", `hide-${value}`]]]] );
+				ջժ(ջլ, [ջժ.update, [[5, "class", `hide-${value}`]]] );
 			});
-			ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "top"], [5, "show", ջթ.bo(() => !this.settings.value, [this.settings])]]]], [ջժ.body, ջլ => {
-				ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "actions"]]]], [ջժ.body, ջլ => {
-					ջժ(ջլ, [ջժ.create, "div", []], [ջժ.body, ջլ => {
-						ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "left"]]]], [ջժ.body, ջլ => {
-							ջժ(ջլ, [ջժ.create, "input", [[ [0, "class", "input"], [0, "placeholder", "Name"], [1, "disabled", ջթ.bo(() => this.readonly.value, [this.readonly])]]]], [ջժ.bind, ["value", "::change", ջթ.bo(() => this.data.name.value, [this.data.name]), ջխ => {this.data.name.value=ջխ}]], [ջժ.append] );
+			ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "top"], [5, "show", ջթ.bo(() => !this.settings.value, [this.settings])]]], [ջժ.body, ջլ => {
+				ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "actions"]]], [ջժ.body, ջլ => {
+					ջժ(ջլ, [ջժ.create, "div", 0, []], [ջժ.body, ջլ => {
+						ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "left"]]], [ջժ.body, ջլ => {
+							ջժ(ջլ, [ջժ.create, "input", 0, [[0, "class", "input"], [0, "placeholder", "Name"], [1, "disabled", ջթ.bo(() => this.readonly.value, [this.readonly])]]], [ջժ.bind, ["value", "::change", ջթ.bo(() => this.data.name.value, [this.data.name]), ջխ => {this.data.name.value=ջխ}]], [ջժ.append] );
 						}], [ջժ.append]);
-						ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "right"]]]], [ջժ.body, ջլ => {
-							ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "buttons"]]]], [ջժ.body, ջլ => {
+						ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "right"]]], [ջժ.body, ջլ => {
+							ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "buttons"]]], [ջժ.body, ջլ => {
 								if(!embedded) {
-									ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button default"], [3, "click", (event, target) => {this.settings.value=true}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-cog"]]]], [ջժ.append] );}], [ջժ.append]);
-									ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button default"], [3, "click", (event, target) => {this.plus()}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-plus"]]]], [ջժ.append] );}], [ջժ.append]);ջթ.bindIfElse(ջլ, [[() => (!this.readonly.value), [this.readonly]]], ջլ =>
+									ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button default"], [3, "click", (event, target) => {this.settings.value=true}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-cog"]]], [ջժ.append] );}], [ջժ.append]);
+									ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button default"], [3, "click", (event, target) => {this.plus()}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-plus"]]], [ջժ.append] );}], [ջժ.append]);ջթ.bindIfElse(ջլ, [[() => (!this.readonly.value), [this.readonly]]], ջլ =>
 									 {
-										ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button primary"], [3, "click", (event, target) => {this.open()}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-share"]]]], [ջժ.append] );}], [ջժ.append]);
+										ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button primary"], [3, "click", (event, target) => {this.open()}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-share"]]], [ջժ.append] );}], [ջժ.append]);
 									});
 										}
-										ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button primary"], [3, "click", (event, target) => {this.save()}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-play"]]]], [ջժ.append] );}], [ջժ.append]);
-								ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button primary"], [3, "click", (event, target) => {this.fullscreen()}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-expand"]]]], [ջժ.append] );}], [ջժ.append]);
+										ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button primary"], [3, "click", (event, target) => {this.save()}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-play"]]], [ջժ.append] );}], [ջժ.append]);
+								ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button primary"], [3, "click", (event, target) => {this.fullscreen()}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-expand"]]], [ջժ.append] );}], [ջժ.append]);
 								if(embedded) {
-									ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button default"], [3, "click", (event, target) => {this.open()}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-external-link-alt"]]]], [ջժ.append] );}], [ջժ.append]);
+									ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button default"], [3, "click", (event, target) => {this.open()}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-external-link-alt"]]], [ջժ.append] );}], [ջժ.append]);
 								}
 									}], [ջժ.append]);
 						}], [ջժ.append]);
 					}], [ջժ.append]);
 				}], [ջժ.append]);
-				ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "editor"]]]], [ջժ.body, ջլ => {
-					ջժ(ջլ, [ջժ.create, "textarea", [[ [1, "value", this.data.editorSource], [3, "documentappend", (event, target) => {this.initSourceEditor(target)}]]]], [ջժ.append] );ջթ.bindIfElse(ջլ, [[() => (this.result.value.error), [this.result]]], ջլ =>
+				ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "editor"]]], [ջժ.body, ջլ => {
+					ջժ(ջլ, [ջժ.create, "textarea", 0, [[1, "value", this.data.editorSource], [3, "documentappend", (event, target) => {this.initSourceEditor(target)}]]], [ջժ.append] );ջթ.bindIfElse(ջլ, [[() => (this.result.value.error), [this.result]]], ջլ =>
 					 {
-						ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "error"]]]], [ջժ.body, ջլ => {
-							ջժ(ջլ, [ջժ.create, "message", [[ [4, "", ջթ.bo(() => this.result.value.error.message.trim(), [this.result])]]]], [ջժ.append] );
+						ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "error"]]], [ջժ.body, ջլ => {
+							ջժ(ջլ, [ջժ.create, "message", 0, [[4, "", ջթ.bo(() => this.result.value.error.message.trim(), [this.result])]]], [ջժ.append] );
 						}], [ջժ.append]);
 					});ջթ.bindIfElse(ջլ, [[() => (this.result.value.warnings && this.result.value.warnings.length), [this.result, this.result]]], ջլ =>
 							 {
-						ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "warning"]]]], [ջժ.body, ջլ => {
+						ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "warning"]]], [ջժ.body, ջլ => {
 							ջթ.bindEach(ջլ, this.result, () => this.result.value.warnings.slice(0, 3) , (ջլ, {message, position: {line, column}}) => {
-								ջժ(ջլ, [ջժ.create, "div", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Line ${line + 1}, Column ${column}: ${message}`]);}], [ջժ.append]);
+								ջժ(ջլ, [ջժ.create, "div", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Line ${line + 1}, Column ${column}: ${message}`]);}], [ջժ.append]);
 							});ջթ.bindIfElse(ջլ, [[() => (this.result.value.warnings.length > 3), [this.result]]], ջլ =>
 							 {
-								ջժ(ջլ, [ջժ.create, "div", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, ջթ.bo(() => `${this.result.value.warnings.length - 3} more...`, [this.result])]);}], [ջժ.append]);
+								ջժ(ջլ, [ջժ.create, "div", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, ջթ.bo(() => `${this.result.value.warnings.length - 3} more...`, [this.result])]);}], [ջժ.append]);
 							});
 								}], [ջժ.append]);
 					});
 						}], [ջժ.append]);
 			}], [ջժ.append]);
-			ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "top settings"], [5, "show", ջթ.bo(() => this.settings.value, [this.settings])]]]], [ջժ.body, ջլ => {
-				ջժ(ջլ, [ջժ.create, "div", [[ [2, "height", "100%"], [2, "background", "white"], [2, "padding", "16px"], [2, "overflow-y", "auto"]]]], [ջժ.body, ջլ => {
-					ջժ(ջլ, [ջժ.create, "div", [[ [2, "float", "right"]]]], [ջժ.body, ջլ => {
-						ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button danger"], [3, "click", (event, target) => {this.settings.value=false}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-times"]]]], [ջժ.append] );}], [ջժ.append]);
+			ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "top settings"], [5, "show", ջթ.bo(() => this.settings.value, [this.settings])]]], [ջժ.body, ջլ => {
+				ջժ(ջլ, [ջժ.create, "div", 0, [[2, "height", "100%"], [2, "background", "white"], [2, "padding", "16px"], [2, "overflow-y", "auto"]]], [ջժ.body, ջլ => {
+					ջժ(ջլ, [ջժ.create, "div", 0, [[2, "float", "right"]]], [ջժ.body, ջլ => {
+						ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button danger"], [3, "click", (event, target) => {this.settings.value=false}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-times"]]], [ջժ.append] );}], [ջժ.append]);
 					}], [ջժ.append]);
-					ջժ(ջլ, [ջժ.create, "h3", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Settings`]);}], [ջժ.append]);
-					ջժ(ջլ, [ջժ.nop], [ջժ.body, ջլ => {var ջծ= /*whitelist="input"*/ /*whitelist="textarea"*/[[ [2, "width", "100%"], [1, "disabled", ջթ.bo(() => this.readonly.value, [this.readonly])]]];
-						ջժ(ջլ, [ջժ.create, "h4", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Description`]);}], [ջժ.append]);
-						ջժ(ջլ, [ջժ.create, "textarea", ջթ.inherit([[ [0, "class", "input"]]], ջծ)], [ջժ.bind, ["value", "::change", ջթ.bo(() => this.data.description.value, [this.data.description]), ջխ => {this.data.description.value=ջխ}]], [ջժ.append] );
-						ջժ(ջլ, [ջժ.create, "h4", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Mode`]);}], [ջժ.append]);
-						ջժ(ջլ, [ջժ.create, "input", ջթ.inherit([[ [0, "class", "input"], [0, "placeholder", "Mode"]]], ջծ)], [ջժ.bind, ["value", "::change", ջթ.bo(() => this.data.mode.value, [this.data.mode]), ջխ => {this.data.mode.value=ջխ}]], [ջժ.append] );
-						ջժ(ջլ, [ջժ.create, "h4", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Header`]);}], [ջժ.append]);
-						ջժ(ջլ, [ջժ.create, "textarea", ջթ.inherit([[ [0, "class", "input font-mono"], [0, "placeholder", "e.g. <script src=\"example.js\"></script>"], [0, "rows", 6]]], ջծ)], [ջժ.bind, ["value", "::change", ջթ.bo(() => this.data.head.value, [this.data.head]), ջխ => {this.data.head.value=ջխ}]], [ջժ.append] );
+					ջժ(ջլ, [ջժ.create, "h3", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Settings`]);}], [ջժ.append]);
+					ջժ(ջլ, [ջժ.nop], [ջժ.body, ջլ => {var ջծ= /*whitelist="input"*/ /*whitelist="textarea"*/[[2, "width", "100%"], [1, "disabled", ջթ.bo(() => this.readonly.value, [this.readonly])]];
+						ջժ(ջլ, [ջժ.create, "h4", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Description`]);}], [ջժ.append]);
+						ջժ(ջլ, [ջժ.create, "textarea", 0, (ջծ).concat([[0, "class", "input"]])], [ջժ.bind, ["value", "::change", ջթ.bo(() => this.data.description.value, [this.data.description]), ջխ => {this.data.description.value=ջխ}]], [ջժ.append] );
+						ջժ(ջլ, [ջժ.create, "h4", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Mode`]);}], [ջժ.append]);
+						ջժ(ջլ, [ջժ.create, "input", 0, (ջծ).concat([[0, "class", "input"], [0, "placeholder", "Mode"]])], [ջժ.bind, ["value", "::change", ջթ.bo(() => this.data.mode.value, [this.data.mode]), ջխ => {this.data.mode.value=ջխ}]], [ջժ.append] );
+						ջժ(ջլ, [ջժ.create, "h4", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Header`]);}], [ջժ.append]);
+						ջժ(ջլ, [ջժ.create, "textarea", 0, (ջծ).concat([[0, "class", "input font-mono"], [0, "placeholder", "e.g. <script src=\"example.js\"></script>"], [0, "rows", 6]])], [ջժ.bind, ["value", "::change", ջթ.bo(() => this.data.head.value, [this.data.head]), ջխ => {this.data.head.value=ջխ}]], [ջժ.append] );
 					}]);
-					ջժ(ջլ, [ջժ.create, "h4", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Saved snippets`]);}], [ջժ.append]);
-					ջժ(ջլ, [ջժ.create, "table", []], [ջժ.body, ջլ => {
-						ջժ(ջլ, [ջժ.create, "tr", []], [ջժ.body, ջլ => {
-							ջժ(ջլ, [ջժ.create, "th", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Name`]);}], [ջժ.append]);
-							ջժ(ջլ, [ջժ.create, "th", [[ [0, "width", 1]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Size`]);}], [ջժ.append]);
-							ջժ(ջլ, [ջժ.create, "th", [[ [0, "width", 1]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Modified`]);}], [ջժ.append]);
-							ջժ(ջլ, [ջժ.create, "th", [[ [0, "width", 1]]]], [ջժ.append] );
+					ջժ(ջլ, [ջժ.create, "h4", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Saved snippets`]);}], [ջժ.append]);
+					ջժ(ջլ, [ջժ.create, "table", 0, []], [ջժ.body, ջլ => {
+						ջժ(ջլ, [ջժ.create, "tr", 0, []], [ջժ.body, ջլ => {
+							ջժ(ջլ, [ջժ.create, "th", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Name`]);}], [ջժ.append]);
+							ջժ(ջլ, [ջժ.create, "th", 0, [[0, "width", 1]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Size`]);}], [ջժ.append]);
+							ջժ(ջլ, [ջժ.create, "th", 0, [[0, "width", 1]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `Modified`]);}], [ջժ.append]);
+							ջժ(ջլ, [ջժ.create, "th", 0, [[0, "width", 1]]], [ջժ.append] );
 						}], [ջժ.append]);
 						ջթ.bindEach(ջլ, this.snippets, () => this.snippets.value , (ջլ, {id, name, source, modified}) => {
-							ջժ(ջլ, [ջժ.create, "tr", []], [ջժ.body, ջլ => {
-								ջժ(ջլ, [ջժ.create, "td", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `${name}`]);}], [ջժ.append]);
-								ջժ(ջլ, [ջժ.create, "td", []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `${source.length}`]);}], [ջժ.append]);
-								ջժ(ջլ, [ջժ.create, "td", [[ [0, "class", "nowrap"]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `${new Date(modified).toLocaleString()}`]);}], [ջժ.append]);
-								ջժ(ջլ, [ջժ.create, "td", []], [ջժ.body, ջլ => {
-									ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "buttons"]]]], [ջժ.body, ջլ => {
-										ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button primary"], [3, "click", (event, target) => {this.load(id)}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-play"]]]], [ջժ.append] );}], [ջժ.append]);
-										ջժ(ջլ, [ջժ.create, "button", [[ [0, "class", "button danger"], [3, "click", (event, target) => {this.remove(name, id)}]]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", [[ [0, "class", "fas fa-trash"]]]], [ջժ.append] );}], [ջժ.append]);
+							ջժ(ջլ, [ջժ.create, "tr", 0, []], [ջժ.body, ջլ => {
+								ջժ(ջլ, [ջժ.create, "td", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `${name}`]);}], [ջժ.append]);
+								ջժ(ջլ, [ջժ.create, "td", 0, []], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `${source.length}`]);}], [ջժ.append]);
+								ջժ(ջլ, [ջժ.create, "td", 0, [[0, "class", "nowrap"]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.text, `${new Date(modified).toLocaleString()}`]);}], [ջժ.append]);
+								ջժ(ջլ, [ջժ.create, "td", 0, []], [ջժ.body, ջլ => {
+									ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "buttons"]]], [ջժ.body, ջլ => {
+										ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button primary"], [3, "click", (event, target) => {this.load(id)}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-play"]]], [ջժ.append] );}], [ջժ.append]);
+										ջժ(ջլ, [ջժ.create, "button", 0, [[0, "class", "button danger"], [3, "click", (event, target) => {this.remove(name, id)}]]], [ջժ.body, ջլ => {ջժ(ջլ, [ջժ.create, "i", 0, [[0, "class", "fas fa-trash"]]], [ջժ.append] );}], [ջժ.append]);
 									}], [ջժ.append]);
 								}], [ջժ.append]);
 							}], [ջժ.append]);
@@ -278,19 +278,19 @@ class Sandbox {
 					}], [ջժ.append]);
 				}], [ջժ.append]);
 			}], [ջժ.append]);
-			ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "bottom"]]]], [ջժ.body, ջլ => {
-				ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "frame"]]]], [ջժ.body, ջլ => {
-					ջժ(ջլ, [ջժ.create, "iframe", [[ [0, "srcdoc", ջթ.bo(() => this.srcdoc.value, [this.srcdoc])]]]], [ջժ.append] );
+			ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "bottom"]]], [ջժ.body, ջլ => {
+				ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "frame"]]], [ջժ.body, ջլ => {
+					ջժ(ջլ, [ջժ.create, "iframe", 0, [[0, "srcdoc", ջթ.bo(() => this.srcdoc.value, [this.srcdoc])]]], [ջժ.append] );
 				}], [ջժ.append]);
-				ջժ(ջլ, [ջժ.create, "div", [[ [0, "class", "source"]]]], [ջժ.body, ջլ => {
-					ջժ(ջլ, [ջժ.create, "textarea", [[ [1, "value", (this.result.value.source && this.result.value.source.contentOnly || "")], [3, "documentappend", (event, target) => {this.initResultEditor(target)}]]]], [ջժ.append] );
+				ջժ(ջլ, [ջժ.create, "div", 0, [[0, "class", "source"]]], [ջժ.body, ջլ => {
+					ջժ(ջլ, [ջժ.create, "textarea", 0, [[1, "value", (this.result.value.source && this.result.value.source.contentOnly || "")], [3, "documentappend", (event, target) => {this.initResultEditor(target)}]]], [ջժ.append] );
 				}], [ջժ.append]);
 			}], [ջժ.append]);
 		}], [ջժ.append])
 	}
 
 	static style() {var ջլ=ջթ.cfa(ջի, arguments, 0);
-		return ջժ(ջլ, [ջժ.create, "style", []], [ջժ.body, ջլ => {ջթ.cabs(ջլ, (ջխ, $) => {var ջկ=ջթ.root();
+		return ջժ(ջլ, [ջժ.create, "style", 0, []], [ջժ.body, ջլ => {ջթ.cabs(ջլ, (ջխ, $) => {var ջկ=ջթ.root();
 			ջկ.value(`position`, `absolute`);
 			ջկ.value(`top, bottom, left, right`, `0`);
 
