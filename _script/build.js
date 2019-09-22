ac1ea5303e1b851f2097cdd507442ee528349207
Kripth <kripth04@gmail.com>
2019-09-22 22:55:27 +0200

    Update build process and styles

--- _script/build.js
+++ _script/build.js
@@ -16,6 +16,8 @@ const sections = [
 
 const nop = () => {};
 
+const commonjs = (filename, data) => transpile({env: "commonjs", filename, mode, es6: true, runtime: sactory + "index"}, data);
+
 // prepare dist
 try {
 	fs.mkdirSync("./_dist");
@@ -37,8 +39,9 @@ if(versions.indexOf(version) == -1) {
 	versions.push(version);
 }
 versions.sort((a, b) => {
-	const num = value => value.split(".").reverse()
-		.map((str, i) => parseInt(str) + i * 1000)
+	const m = [1000000, 1000, 1];
+	const num = value => value.split(".")
+		.map((str, i) => parseInt(str) * m[i])
 		.reduce((acc, value) => acc + value, 0);
 	return num(b) - num(a);
 });
@@ -51,47 +54,54 @@ const write = (file, content) => {
 };
 
 // compile widgets
-fs.readdirSync("./_src/widget").forEach(filename => {
-	fs.writeFileSync("./_dist/" + filename.slice(0, -2) + "js", transpile({env: "commonjs", filename, mode, es6: true, runtime: sactory + "index"}, fs.readFileSync("./_src/widget/" + filename, "utf8")));
-});
-
-// compile static pages
-const base = "./_src/static/";
-const dist = "./_dist/";
-const static = (folder, parents) => {
-	const get = fname => {
-		const filename = folder.substr(base.length).replace(/\//g, "_") + fname;
-		fs.writeFileSync(dist + filename + ".js", transpile({env: "commonjs", filename, mode, es6: true, runtime: sactory + "index"}, fs.readFileSync(folder + fname + ".sx", "utf8")));
-		return require("../_dist/" + filename);
-	};
-	const root = (parents.map(() => "..").join("/") || ".") + "/";
-	if(fs.existsSync(folder + "_template.sx")) {
-		// add parent
-		parents = parents.concat(get("_template"));
-	}
-	fs.readdirSync(folder).reverse().forEach(fname => {
-		const stat = fs.statSync(folder + "/" + fname);
+const widgets = (folder, paths) => {
+	fs.readdirSync(folder).forEach(filename => {
+		const stat = fs.statSync(folder + "/" + filename);
 		if(stat.isDirectory()) {
-			static(folder + fname + "/", parents);
-		} else if(stat.isFile()) {
-			let attrs = {
-				version, root, sections,
-				path: folder.substr(base.length) + fname.slice(0, -3)
-			};
-			if(fname.endsWith(".blank.sx")) {
-				const widget = get(fname.slice(0, -3));
-				write(folder.substr(base.length) + fname.slice(0, -8) + "html", new widget().render(attrs).render());
-			} else if(fname != "_template.sx") {
-				const widget = get(fname.slice(0, -3));
-				write(folder.substr(base.length) + fname.slice(0, -2) + "html", new parents[0]({
-					...attrs,
-					children: parents.slice(1).concat(widget)
-				}).render().render());
-			}
+			widgets(folder + filename + "/", paths.concat(filename));
+		} else {
+			fs.writeFileSync("./_dist/" + paths.map(p => p + "_").join("") + filename.slice(0, -2) + "js", transpile({env: "commonjs", filename, mode, es6: true, runtime: sactory + "index"}, fs.readFileSync(folder + filename, "utf8")));
 		}
 	});
 };
-static(base, []);
+widgets("./_src/widget/", []);
+
+// compile static pages
+const render = require("../_dist/render");
+const base = "./_src/static/";
+const dist = "./_dist/";
+const static = folder => {
+	const root = (folder == base ? "." : folder.substr(base.length).split("/").map(() => "..").join("/")) + "/";
+	fs.readdirSync(folder).forEach(fname => {
+		const stat = fs.statSync(folder + "/" + fname);
+		if(stat.isDirectory()) {
+			static(folder + fname + "/");
+		} else if(stat.isFile()) {
+			const attrs = {
+				version, root, sections,
+				path: folder.substr(base.length) + fname.slice(0, -3)
+			};
+			const filename = "static_" + folder.substr(base.length).replace(/\//g, "_") + fname.slice(0, -3);
+			fs.writeFileSync(dist + filename + ".js", commonjs(filename, fs.readFileSync(folder + fname, "utf8")));
+			const Widget = require("../_dist/" + filename);
+			write(folder.substr(base.length) + fname.slice(0, -3), render(Widget, attrs));
+		}
+	});
+};
+static(base);
+
+// compile tour and guide
+const Tour = require("../_dist/tour");
+const Guide = require("../_dist/guide");
+sections.forEach(([section, subs]) => {
+	let contents = [];
+	subs.forEach(sub => {
+		const content = require(`../_dist/tour_${section}_${sub}`);
+		contents.push({sub, content});
+		write(`./tour/${section}/${sub}.html`, render(Tour, {root: "../../", version, sections, section, sub, content}));
+	});
+	write(`./guide/${section}.html`, render(Guide, {root: "../", version, sections, section, contents}));
+});
 
 // compile scripts
 fs.readdirSync("./_src/dist").forEach(fname => {
@@ -113,7 +123,7 @@ fs.readdirSync(sactoryDist).forEach(filename => {
 });
 
 // compile css
-//require("../../sactify/src/build")("./_src/style", "./css");
+require("../../sactify/src/build")("./_src/style", "./css");
 
 // copy resources
 ncp("./_src/res", "./res");
@@ -123,7 +133,7 @@ ncp("./css", `./v/${version}/css`);
 ncp("./res", `./v/${version}/res`);
 
 // write global script for creating the version select
-fs.writeFile("./versions.js", `
+fs.writeFile("./dist/versions.js", `
 window.addEventListener("load", function(){
 	var select = document.querySelector(".header select");
 	var value = select.value;
@@ -135,7 +145,15 @@ window.addEventListener("load", function(){
 	});
 	select.value = value;
 	select.addEventListener("change", function(){
-		//TODO switch version
+		var version = "/v/" + this.value;
+		var path = window.location.href.substr(window.location.origin.length);
+		var match = path.match(/^\\/v\\/\\d+\\.\\d+\\.\\d+\\//);
+		if(match) {
+			path = version + "/" + path.substr(match[0].length);
+		} else {
+			path = version + path;
+		}
+		window.location.href = path;
 	});
 });
-`, nop);
+`.replace(/[\n\r\t]/gm, ""), nop);
